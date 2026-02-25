d58b39e2783bfc08e7bd5d5ab7590b844ae5af99
Andikapratama20 <kerjajaya67@gmail.com>
2026-02-26 00:01:15 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
-export const metadata = {
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
   title: 'PT. ANDIKA PRATAMA',
   description: 'Solusi Kerja Premium',
 }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="id">
       <body style={{ margin: 0, fontFamily: 'Arial, sans-serif' }}>
